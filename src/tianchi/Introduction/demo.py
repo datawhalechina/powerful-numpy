eca2ac0f7058f5f770fd5113c6b4590c319c6f3a
hscspring <haoshaochun@gmail.com>
2022-03-26 20:13:48 +0800

    update

---
+++ src/tianchi/Introduction/demo.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+a = 10
+b = 10
+res = add(a, b)
+print(res)
--- src/tianchi/demo.py
+++
@@ -1,8 +0,0 @@
-def add(a: int, b: int) -> int:
-    return a + b
-
-
-a = 10
-b = 10
-res = add(a, b)
-print(res)
